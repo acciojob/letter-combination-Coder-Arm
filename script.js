f81866807518cf6e75d029aa4cc04537dfef86db
Coder-Arm <113359996+Coder-Arm@users.noreply.github.com>
2023-06-16 15:07:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function letterCombinations(input_digit) {
   //Complete the function
+	const lookup = {
+		"0" : 0,
+	    "1" : 1,
+		"2" : "abc",
+		"3" : "def",
+	    "4" : "ghi",
+		"5" : "jkl",
+		"6" : "mno",
+		"7" : "pqrs",
+		"8" : "tuv",
+		"9" : "wxyz"
+	}
+
+	function calculate(currIdx,str){
+		
+	}
+	
+
 }
 
 module.exports = letterCombinations;
